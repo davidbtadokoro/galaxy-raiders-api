8d55d162f0ff716b197a3850b5eb1ec757dbda99
David de Barros Tadokoro <davidbtadokoro@usp.br>
2022-07-23 02:26:56 -0300

    Updates SpaceField class (referent to Explosion class implementation)

--- app/src/main/kotlin/galaxyraiders/core/game/SpaceField.kt
+++ app/src/main/kotlin/galaxyraiders/core/game/SpaceField.kt
@@ -56,7 +56,7 @@ data class SpaceField(val width: Int, val height: Int, val generator: RandomGene
   }
 
   private fun standardShipPosition(): Point2D {
-    return Point2D(x = this.width / 2.0, y = 1.0)
+    return Point2D(x = 1.0, y = 1.0)
   }
 
   private fun standardShipVelocity(): Vector2D {
@@ -73,11 +73,13 @@ data class SpaceField(val width: Int, val height: Int, val generator: RandomGene
   }
 
   private fun defineMissilePosition(missileRadius: Double): Point2D {
-    return ship.center + Vector2D(dx = 0.0, dy = ship.radius + missileRadius + SpaceFieldConfig.missileDistanceFromShip)
+    return ship.center + Vector2D(
+      dx = ship.radius + missileRadius + SpaceFieldConfig.missileDistanceFromShip,
+      dy = 0.0)
   }
 
   private fun defineMissileVelocity(): Vector2D {
-    return Vector2D(dx = 0.0, dy = 1.0)
+    return Vector2D(dx = 1.0, dy = 0.0)
   }
 
   private fun createAsteroidWithRandomProperties(): Asteroid {
@@ -126,4 +128,19 @@ data class SpaceField(val width: Int, val height: Int, val generator: RandomGene
 
     return scaledMass * SpaceFieldConfig.asteroidMassMultiplier
   }
+
+  /* ---------------------------- EP ---------------------------- */
+
+  var explosions: List<Explosion> = emptyList()
+    private set
+
+  fun generateExplosion(pointOfExplosion: Point2D) {
+    explosions += createExplosion(pointOfExplosion)
+  }
+
+  private fun createExplosion(pointOfExplosion: Point2D): Explosion {
+    return Explosion(pointOfExplosion)
+  }
+
+  /* ---------------------------- EP ---------------------------- */
 }

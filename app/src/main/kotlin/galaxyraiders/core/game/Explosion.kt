6a1d88508f718f7aba9c6fb0bf1787f92a25062e
David de Barros Tadokoro <davidbtadokoro@usp.br>
2022-07-24 14:41:49 -0300

    Adds comments to the Explosion class

--- app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
+++ app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
@@ -3,6 +3,16 @@ package galaxyraiders.core.game
 import galaxyraiders.core.physics.Point2D
 import galaxyraiders.core.physics.Vector2D
 
+/*
+ *  Note: as the explosion is static (does not moves) nor it should be
+ *  a factor in any collision (does not have mass), we hardcoded some
+ *  properties of the class:
+ *  - Its initial velocity is set to 0 (Vector2D(0.0, 0.0));
+ *  - Its radius is set to 2.0 (results in a good sized explosion asset);
+ *  - Its mass is set to 0.0 (as explosions are energy and therefore do
+ *  not have any mass).
+ */
+
 class Explosion(pointOfExplosion: Point2D) :
   SpaceObject("Explosion", '#', pointOfExplosion, Vector2D(0.0, 0.0), 2.0, 0.0) {
 
